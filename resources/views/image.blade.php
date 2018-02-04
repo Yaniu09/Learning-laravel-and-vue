8815195f84f65c34ebc191ad58750553b8088cae
Yaniu09 <imperialaeiou@gmail.com>
2018-02-04 21:08:42 +0500

    asd

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -9,6 +9,24 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">My Blog</a>
+        
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="text" placeholder="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
 
     <div class="container">
       <div class="row">
@@ -20,8 +38,15 @@
                 <img src="https://source.unsplash.com/random" class="card-img-top" alt="image not foun">
               @endif  
               <div class="card-body">
-                <h4 class="card-title">Maldives - Oceans and Seas; Riding waves</h4>
-                <p class="card-text">Body</p>
+                <h4 class="card-title">Maldives - Oceans and Seas; Riding wavesteam</h4>
+                <p class="card-text minimize">
+                    An unforgettable initial impression upsurges your curiosity as soon as you step out of the flight at any of the 11 airports in Maldives; Velana International Airport (main airport of Maldives), Gan International Airport, Hanimaadhoo International Airport, Kaadedhdhoo Airport, Kadhdhoo Airport, Villa International Airport, Dharavandhoo Airport, Kooddoo Airport, Thimarafushi Airport, Fuvahmulah Aiport and Dhaalu Airport. 
+                    <br>
+                    Airports are the pressure points in most of our lives, however, if you choose to vacate or explore or even breath out all the pressure and negativity out of your soul, Maldives is definitely going to fulfill all your dreams by turning your airport experiences into memorable ones. Choose your way of travelling to any of the 26 ring-shaped atolls; 1192 islands; hop on a ferry or a speedboat if not a seaplane. Domestic flights are also a way of transfer if you prefer. Being a Maldivian, I would recommend you to take a deep breath and step on a speedboat/ferry; it would definitely change your lives forever. Specially, if you are a budget traveler then never miss the opportunity to ride on a ferry anywhere on this territory spread over 90,000 square kilometers; one of the most distinct places on Earth? 
+                    <br>
+                    Over 1000 coral islands spread over an ocean, each one with its own nature, different and unique from each other, Maldives could become your perfect escape. A wide country which is unexplored by backpackers has been gaining its popularity among backpackers and budget travelers as all the atolls are connected to each other by a ferry system. Public ferry services could be a lot cheaper while chartered transfers are also an option. Nevertheless, resorts and private picnic islands are excluded from the public ferry service; they offer speedboat transfers though. 
+                    Putting it simply, Maldives consists of 99.9% water, and it is a part of who we are; why not choose to breathe out all your stress and negativity over the oceans and seas by riding the waves! Lush greenery and pristine beaches represent less than 1% of all, and together they have been an endless source of inspiration for humanity; nature lovers.  
+                </p>
               </div>
             </div>
         </div>
@@ -33,8 +58,15 @@
                 <img src="https://source.unsplash.com/random" class="card-img-top" alt="image not foun">
               @endif  
               <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Body</p>
+                <h4 class="card-title">Maldives; a wondrous archipelago</h4>
+                <p class="card-text minimize">
+                    A sequence of sprinkled islands, grouped in a double chain of 26 atolls are composed of live coral reefs and sand bars; the most complex and vibrant structures of the planet. Being the lowest lying country in the world, Maldives never failed to gain popularity among the travelers across the globe for its breathtaking landscape; anywhere you step your feet you get to feel the extensive natural beauty while experiencing the wonderful world “WE” live in. 
+                    
+                    We live in a wondrous world that is full of beauty, charm and adventure. There is no end or limit to the adventures that you can have in this tropical paradise. The paradise itself is composed naturally of living corals forming ring shaped reefs which comprises a heaven of marine life. 
+                    Geologically it is said that the filling up of the lagoons of the ring-shaped reefs by reef sediments has resulted in the formation of coral islands. Built diligently over thousands of years, the physical features of these islands and their relation to its geological structures varies tremendously in different atolls. The extraordinary archipelago of these isles have been gaining fame among explorers; the physical setting of the atolls, reefs, lagoons, islands and the marine biodiversity here is all-embracing without any limits! 
+                    With a population of around 350,000 people in 200 islands of around 2000 islands, Maldives is a country with more territorial sea than dry land. Hence, the coral reefs which built the country play vital roles in the economic and social well-being of the community; we rely on healthy reefs for our existence. The remoteness of the islands and the wide distribution of reefs are found in pristine conditions and comprises the most complex reef systems in the world. Almost all the atolls have unusual ring-shaped house reefs within the atoll lagoons, each with its own sandy lagoons and rims of lining corals. Maldives, the most diverse of all marine ecosystems has a bewildering array of other organisms associated with reefs making it the most diverse and species rich area that exist in the marine world with a total of over 1000 species of fish; 7 of them have been described as new to science with several more awaiting for description. 
+                    The lush greenery would moreover captivate your soul making you forget to blink your eyes. The islands are hidden amidst tall and graceful coconut palms standing proudly above all other trees. Known as the ‘Tree of Life’ the coconut palm which is also considered to be one of the 10 most useful trees in the world is the ‘Life of Maldives’. Emerald green palm trees swaying in the breeze gives you natural sunshade anywhere on an island, sometimes even on the beaches.  
+                </p>
               </div>
             </div>
         </div>
@@ -83,6 +115,36 @@
           console.log('Server response (Failed): ' + textStatus);
         });
       }
+    </script>
+     <script>
+        jQuery(function(){
+
+            var minimized_elements = $('p.minimize');
+            
+            minimized_elements.each(function(){    
+                var t = $(this).text();        
+                if(t.length < 300) return;
+                
+                $(this).html(
+                    t.slice(0,300)+'<span>... </span><a href="#" class="more" style="color:black;"> +Show More</a>'+
+                    '<span style="display:none; color:black;">'+ t.slice(100,t.length)+' <a href="#" class="less" style="color:black;"> -Show Less</a></span>'
+                );
+                
+            }); 
+            
+            $('a.more', minimized_elements).click(function(event){
+                event.preventDefault();
+                $(this).hide().prev().hide();
+                $(this).next().show();        
+            });
+            
+            $('a.less', minimized_elements).click(function(event){
+                event.preventDefault();
+                $(this).parent().hide().prev().show().prev().show();    
+            });
+
+        });
+
     </script>
   </body>
 </html>
